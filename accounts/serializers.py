f1d6bc6c06c393864e101b67894b50db79669547
charmniovi <charmniovi@tutanota.com>
2024-06-07 16:15:23 +0300

    appended "first_name", "last_name" in UserSerializer.Meta.fields

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("username", "email", "password")
+        fields = ("username", "email", "password", "first_name", "last_name")
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
