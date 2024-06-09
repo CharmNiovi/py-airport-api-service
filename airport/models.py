f90633978d270d185d2f5f31007a6bc27a9b3bd5
charmniovi <charmniovi@tutanota.com>
2024-06-09 17:48:31 +0300

    create Airplane model

--- airport/models.py
+++ airport/models.py
@@ -9,3 +9,27 @@ class AirplaneType(models.Model):
     def __str__(self):
         return self.name
 
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=100)
+    rows = models.IntegerField(validators=[MinValueValidator(1)])
+    seats_per_row = models.IntegerField(validators=[MinValueValidator(1)])
+    airplane_type = models.ForeignKey(AirplaneType,
+                                      on_delete=models.CASCADE,
+                                      related_name="airplanes")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["name", "airplane_type"],
+                name="unique_airplane"
+            ),
+        ]
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def capacity(self):
+        return self.rows * self.seats_per_row
+
