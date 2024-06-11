86284f70fd40c921cfbfe4e5c1c8d982cfc4d18f
charmniovi <charmniovi@tutanota.com>
2024-06-11 18:34:38 +0300

    added CountryViewSet

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.mixins import (
+    ListModelMixin,
+    RetrieveModelMixin,
+    CreateModelMixin,
+    DestroyModelMixin
+)
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import Response
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from airport.models import (
+    City,
+    Country,
+    AirplaneType,
+    Airplane,
+    Route,
+    Crew,
+    Airport,
+    Order,
+    Flight,
+    Ticket,
+)
+from airport.serializers import (
+    CitySerializer,
+    CityWithSlugSerializer,
+    CountrySerializer,
+    AirplaneTypeSerializer,
+    AirplaneTypeDetailSerializer,
+    AirplaneListSerializer,
+    AirplaneSerializer,
+    AirplaneRetrieveSerializer,
+    AirportSerializer,
+    AirportListSerializer,
+    AirportDetailSerializer,
+    RouteSerializer,
+    RouteWithSlugSerializer,
+    CrewSerializer,
+    CrewDetailSerializer,
+    OrderAdminSerializer,
+    OrderUserSerializer,
+    OrderDetailSerializer,
+    FlightSerializer,
+    FlightDetailSerializer,
+    FlightListSerializer,
+    TicketSerializer,
+    TicketDetailSerializer,
+    TicketUnableToBuySerializer,
+)
+from core.permissions import IsAdminOrReadOnly, OrderSpecialPermission
+
+
+class CountryViewSet(ModelViewSet):
+    serializer_class = CountrySerializer
+    queryset = Country.objects.all()
+    permission_classes = (IsAdminOrReadOnly,)
+    authentication_classes = (JWTAuthentication,)
+
