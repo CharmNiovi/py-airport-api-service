9e4e3a3fefb94f1c0f12523bc2fac1fc802c5a54
charmniovi <charmniovi@tutanota.com>
2024-06-11 18:27:46 +0300

    added IsAdminOrReadOnly and OrderSpecialPermission permissions

---
+++ core/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user.is_staff or request.user.is_superuser
+
+
+class OrderSpecialPermission(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        if request.method == "POST":
+            return True
+        return request.user.is_staff or request.user.is_superuser
